08f33558b867d5538cc79cd8729c8f9201b8a2bd
HeatherEWhite <46779842+HeatherEWhite@users.noreply.github.com>
2020-08-03 16:19:23 +0100

    Update PCA.R

--- Code_for_analyses/PCA.R
+++ Code_for_analyses/PCA.R
@@ -4,7 +4,7 @@
 
 # Date created: 22/01/20
 
-# Last modified: 24/04/20
+# Last modified: 24/05/20
 
 # License: MIT license
 
@@ -35,11 +35,11 @@ library(svd)
 # Reading in the landmarks and adding to an array for later analysis
 
 
-# Reading in the landmarks
+# Read in the landmarks
 folder <- 'Data/Resampled_landmarks'
 
 
-# Number of specimens I have
+# Number of specimens
 nspecimens<-79
 # Read in the file names to a list variable:
 csvlist <- list.files(path = folder, pattern = "*.csv", full.names = T)
@@ -63,7 +63,7 @@ lm_array
 dim(lm_array)
 
 
-# Reading in the species names .csv - this is in the same order as the specimens
+# Reading in the species names .csv - this needs to be in the same order as the specimens
 species_names <- read.csv('Data/Species_names_ordered.csv', header = F)
 dimnames(lm_array)[3]=species_names
 
@@ -90,10 +90,9 @@ PCA=plotTangentSpace(test, axis1=1, axis2=2, warpgrids = F)
 
 
 # Plotting the PCA with a heatmap for complexity scores
-# Shows if there is grouping based on high complexity and morphology
 
 
-# Read in complexity data
+# Read in the complexity data
 complexity <- read.csv('Analysis/all_complexity_results.csv')
 # Extract the data for each method
 FD_box <- as.numeric(complexity[,2]) 
@@ -132,7 +131,7 @@ PCA=plotTangentSpace(test, groups= col.gp, axis1=1, axis2=2, warpgrids = F, labe
 PC_scores <- PCA$pc.scores
 
 # Writing PC score results for each specimen to a csv
-write.csv(PC_scores, "Y:/Heather/R_Projects/suture_methods_comparison/Analysis/PC_scores.csv")
+write.csv(PC_scores, "Y:/xxx/xxx/xxx.csv")
 
 
 ######################################################################################################
@@ -140,7 +139,7 @@ write.csv(PC_scores, "Y:/Heather/R_Projects/suture_methods_comparison/Analysis/P
 # Other useful information from the PCA
 
 
-# Accessing the minimum PC shape for PC1 - the extreme end, so I can plot this and the max end for on PCA figure
+# Accessing the minimum PC shape for PC1
 PCA$pc.shapes$PC1min
 
 # Summary of contributions from each PC axis
@@ -182,11 +181,8 @@ data <- read.csv('Analysis/all_complexity_results_with_PC_scores.csv', header = 
 data
 
 
-# Corrplot allows a comparison between the methods, shows if they are similarly related or not.
-# This is better than doing regression analysis as regression analysis relies on which you choose as predictor or dependent
-
 # Get the necessary data
-# Five method results and PC1, PC2, PC3, PC4 (all >5% variation)
+# Five method results and PC1, PC2, PC3, PC4 (PCs with >5% variation)
 data2 <- dplyr::select(data, 2:10)
 
 # Input data 'data' = matrix with specimens listed and every method result for each specimen
@@ -202,537 +198,11 @@ corrplot(cor2, method = "color")
 corrplot(cor, p.mat = res1$p, sig.level = .05)
 
 
-write.csv(cor2, "/Users/heatherwhite/Documents/PhD/PhD/R_Projects/suture_methods_comparison/Analysis/complexity_vs_PC_score_correlations.csv")
+write.csv(cor2, "Y:/xxx/xxx/xxx.csv")
 
 
 
 
-#############################################################################################
-
-# Bivariate analysis - relationship between method and PC scores from shape data
-
-# This was the analysis done for the original manuscript
-
-
-# Plotting PC1 scores alongside SI result
-ggplot(data, aes(x = PC1, y = SI))+ 
-  geom_point() +
-  xlab("PC1 score") +
-  ylab("Sinuosity Index")+
-  theme_bw()
-
-# Fit the general linear model
-model1 <- lm(PC1 ~ SI, data = data)
-
-# Evaulating the assumptions of the model
-par(mfrow = c(2,2))
-plot(model1) 
-mtext("Model assumptions (PC1 and SI)", outer=T, cex=1, line=-2)
-par(mfrow = c(1,1))
-
-# To get a summary of the model
-summary(model1)
-# To get the anova summary (Pr(>F) = p-value) 
-# I don't think this anova bit is necessary
-anova(model1)
-
-# Add the model interpretation to the scatter plot (ie the line)
-ggplot(data, aes(x = PC1, y = SI))+ 
-  geom_point(colour = "cornflowerblue") +
-  geom_smooth(method = 'lm', colour = "black", size = 1) +
-  xlab("PC1 score") +
-  ylab("Sinuosity Index")+
-  theme_bw(base_size = 14)
-
-
-
-#################################################
-
-
-# Plotting PC2 scores alongside SI result
-ggplot(data, aes(x = PC2, y = SI))+ 
-  geom_point() +
-  xlab("PC2 score") +
-  ylab("Sinuosity Index")+
-  theme_bw()
-
-# Fit the general linear model
-model2 <- lm(PC2 ~ SI, data = data)
-
-# Evaulating the assumptions of the model
-par(mfrow = c(2,2))
-plot(model2) 
-mtext("Model assumptions (PC2 and SI)", outer=T, cex=1, line=-2)
-par(mfrow = c(1,1))
-
-# To get a summary of the model
-summary(model2)
-# To get the anova summary (Pr(>F) = p-value) 
-# I don't think this anova bit is necessary
-anova(model2)
-
-# Add the model interpretation to the scatter plot (ie the line)
-ggplot(data, aes(x = PC2, y = SI))+ 
-  geom_point(colour = "cornflowerblue") +
-  geom_smooth(method = 'lm', colour = "black", size = 1) +
-  xlab("PC2 score") +
-  ylab("Sinuosity Index")+
-  theme_bw(base_size = 14)
-
-
-#################################################
-
-
-# Plotting PC1 scores alongside SCI result
-ggplot(data, aes(x = PC1, y = SCI))+ 
-  geom_point() +
-  xlab("PC1 score") +
-  ylab("Suture Complexity Index")+
-  theme_bw()
-
-# Fit the general linear model
-model3 <- lm(PC1 ~ SCI, data = data)
-
-# Evaulating the assumptions of the model
-par(mfrow = c(2,2))
-plot(model3) 
-mtext("Model assumptions (PC1 and SCI)", outer=T, cex=1, line=-2)
-par(mfrow = c(1,1))
-
-# To get a summary of the model
-summary(model3)
-# To get the anova summary (Pr(>F) = p-value) 
-# I don't think this anova bit is necessary
-anova(model3)
-
-# Add the model interpretation to the scatter plot (ie the line)
-ggplot(data, aes(x = PC1, y = SCI))+ 
-  geom_point(colour = "cornflowerblue") +
-  geom_smooth(method = 'lm', colour = "black", size = 1) +
-  xlab("PC1 score") +
-  ylab("Suture Complexity Index")+
-  theme_bw(base_size = 14)
-
-
-
-
-#################################################
-
-
-# Plotting PC2 scores alongside SCI result
-ggplot(data, aes(x = PC2, y = SCI))+ 
-  geom_point() +
-  xlab("PC2 score") +
-  ylab("Suture Complexity Index")+
-  theme_bw()
-
-# Fit the general linear model
-model4 <- lm(PC2 ~ SCI, data = data)
-
-# Evaulating the assumptions of the model
-par(mfrow = c(2,2))
-plot(model4) 
-mtext("Model assumptions (PC2 and SCI)", outer=T, cex=1, line=-2)
-par(mfrow = c(1,1))
-
-# To get a summary of the model
-summary(model4)
-# To get the anova summary (Pr(>F) = p-value) 
-# I don't think this anova bit is necessary
-anova(model4)
-
-# Add the model interpretation to the scatter plot (ie the line)
-ggplot(data, aes(x = PC2, y = SCI))+ 
-  geom_point(colour = "cornflowerblue") +
-  geom_smooth(method = 'lm', colour = "black", size = 1) +
-  xlab("PC2 score") +
-  ylab("Suture Complexity Index")+
-  theme_bw(base_size = 14)
-
-
-
-
-#################################################
-
-# Plotting PC1 scores alongside FD box result
-ggplot(data, aes(x = PC1, y = FD_box))+ 
-  geom_point() +
-  xlab("PC1 score") +
-  ylab("Fractal Dimension (box counting)")+
-  theme_bw()
-
-# Fit the general linear model
-model5 <- lm(PC1 ~ FD_box, data = data)
-
-# Evaulating the assumptions of the model
-par(mfrow = c(2,2))
-plot(model5) 
-mtext("Model assumptions (PC1 and FD box)", outer=T, cex=1, line=-2)
-par(mfrow = c(1,1))
-
-# To get a summary of the model
-summary(model5)
-# To get the anova summary (Pr(>F) = p-value) 
-# I don't think this anova bit is necessary
-anova(model5)
-
-# Add the model interpretation to the scatter plot (ie the line)
-ggplot(data, aes(x = PC1, y = FD_box))+ 
-  geom_point(colour = "cornflowerblue") +
-  geom_smooth(method = 'lm', colour = "black", size = 1) +
-  xlab("PC1 score") +
-  ylab("Fractal Dimension (box counting)")+
-  theme_bw(base_size = 14)
-
-
-
-
-#################################################
-
-# Plotting PC2 scores alongside FD box result
-ggplot(data, aes(x = PC2, y = FD_box))+ 
-  geom_point() +
-  xlab("PC2 score") +
-  ylab("Fractal Dimension (box counting)")+
-  theme_bw()
-
-# Fit the general linear model
-model6 <- lm(PC2 ~ FD_box, data = data)
-
-# Evaulating the assumptions of the model
-par(mfrow = c(2,2))
-plot(model6) 
-mtext("Model assumptions (PC2 and FD_box)", outer=T, cex=1, line=-2)
-par(mfrow = c(1,1))
-
-# To get a summary of the model
-summary(model6)
-# To get the anova summary (Pr(>F) = p-value) 
-# I don't think this anova bit is necessary
-anova(model6)
-
-# Add the model interpretation to the scatter plot (ie the line)
-ggplot(data, aes(x = PC2, y = FD_box))+ 
-  geom_point(colour = "cornflowerblue") +
-  geom_smooth(method = 'lm', colour = "black", size = 1) +
-  xlab("PC2 score") +
-  ylab("Fractal Dimension (box counting)")+
-  theme_bw(base_size = 14)
-
-
-
-
-#################################################
-
-# Plotting PC1 scores alongside FD madogram result
-ggplot(data, aes(x = PC1, y = FD_madogram))+ 
-  geom_point() +
-  xlab("PC1 score") +
-  ylab("Fractal Dimension (madogram)")+
-  theme_bw()
-
-# Fit the general linear model
-model7 <- lm(PC1 ~ FD_madogram, data = data)
-
-# Evaulating the assumptions of the model
-par(mfrow = c(2,2))
-plot(model7) 
-mtext("Model assumptions (PC1 and FD madogram)", outer=T, cex=1, line=-2)
-par(mfrow = c(1,1))
-
-# To get a summary of the model
-summary(model7)
-# To get the anova summary (Pr(>F) = p-value) 
-# I don't think this anova bit is necessary
-anova(model7)
-
-# Add the model interpretation to the scatter plot (ie the line)
-ggplot(data, aes(x = PC1, y = FD_madogram))+ 
-  geom_point(colour = "cornflowerblue") +
-  geom_smooth(method = 'lm', colour = "black", size = 1) +
-  xlab("PC1 score") +
-  ylab("Fractal Dimension (madogram)")+
-  theme_bw(base_size = 14)
-
-
-
-
-#################################################
-
-# Plotting PC2 scores alongside FD madogram result
-ggplot(data, aes(x = PC2, y = FD_madogram))+ 
-  geom_point() +
-  xlab("PC2 score") +
-  ylab("Fractal Dimension (madogram)")+
-  theme_bw()
-
-# Fit the general linear model
-model8 <- lm(PC2 ~ FD_madogram, data = data)
-
-# Evaulating the assumptions of the model
-par(mfrow = c(2,2))
-plot(model8) 
-mtext("Model assumptions (PC2 and FD madogram)", outer=T, cex=1, line=-2)
-par(mfrow = c(1,1))
-
-# To get a summary of the model
-summary(model8)
-# To get the anova summary (Pr(>F) = p-value) 
-# I don't think this anova bit is necessary
-anova(model8)
-
-# Add the model interpretation to the scatter plot (ie the line)
-ggplot(data, aes(x = PC2, y = FD_madogram))+ 
-  geom_point(colour = "cornflowerblue") +
-  geom_smooth(method = 'lm', colour = "black", size = 1) +
-  xlab("PC2 score") +
-  ylab("Fractal Dimension (madogram)")+
-  theme_bw(base_size = 14)
-
-
-
-
-#################################################
-
-# Plotting PC1 scores alongside PSD result
-ggplot(data, aes(x = PC1, y = PSD))+ 
-  geom_point() +
-  xlab("PC1 score") +
-  ylab("Power Spectrum Density")+
-  theme_bw()
-
-# Fit the general linear model
-model9 <- lm(PC1 ~ PSD, data = data)
-
-# Evaulating the assumptions of the model
-par(mfrow = c(2,2))
-plot(model9) 
-mtext("Model assumptions (PC1 and PSD)", outer=T, cex=1, line=-2)
-par(mfrow = c(1,1))
-
-# To get a summary of the model
-summary(model9)
-# To get the anova summary (Pr(>F) = p-value) 
-# I don't think this anova bit is necessary
-anova(model9)
-
-# Add the model interpretation to the scatter plot (ie the line)
-ggplot(data, aes(x = PC1, y = PSD))+ 
-  geom_point(colour = "cornflowerblue") +
-  geom_smooth(method = 'lm', colour = "black", size = 1) +
-  xlab("PC1 score") +
-  ylab("Power Spectrum Density")+
-  theme_bw(base_size = 14)
-
-
-
-
-#################################################
-
-# Plotting PC2 scores alongside PSD result
-ggplot(data, aes(x = PC2, y = PSD))+ 
-  geom_point() +
-  xlab("PC2 score") +
-  ylab("Power Spectrum Density")+
-  theme_bw()
-
-# Fit the general linear model
-model10 <- lm(PC2 ~ PSD, data = data)
-
-# Evaulating the assumptions of the model
-par(mfrow = c(2,2))
-plot(model10) 
-mtext("Model assumptions (PC2 and PSD)", outer=T, cex=1, line=-2)
-par(mfrow = c(1,1))
-
-# To get a summary of the model
-summary(model10)
-# To get the anova summary (Pr(>F) = p-value) 
-# I don't think this anova bit is necessary
-anova(model10)
-
-# Add the model interpretation to the scatter plot (ie the line)
-ggplot(data, aes(x = PC2, y = PSD))+ 
-  geom_point(colour = "cornflowerblue") +
-  geom_smooth(method = 'lm', colour = "black", size = 1) +
-  xlab("PC2 score") +
-  ylab("Power Spectrum Density")+
-  theme_bw(base_size = 14)
-
-
-
-
-#################################################
-
-
-# Plotting PC3 scores alongside SI result
-ggplot(data, aes(x = PC3, y = SI))+ 
-  geom_point() +
-  xlab("PC3 score") +
-  ylab("Sinuosity Index")+
-  theme_bw()
-
-# Fit the general linear model
-model11 <- lm(PC3 ~ SI, data = data)
-
-# Evaulating the assumptions of the model
-par(mfrow = c(2,2))
-plot(model11) 
-mtext("Model assumptions (PC3 and SI)", outer=T, cex=1, line=-2)
-par(mfrow = c(1,1))
-
-# To get a summary of the model
-summary(model11)
-# To get the anova summary (Pr(>F) = p-value) 
-# I don't think this anova bit is necessary
-anova(model11)
-
-# Add the model interpretation to the scatter plot (ie the line)
-ggplot(data, aes(x = PC3, y = SI))+ 
-  geom_point(colour = "cornflowerblue") +
-  geom_smooth(method = 'lm', colour = "black", size = 1) +
-  xlab("PC3 score") +
-  ylab("Sinuosity Index")+
-  theme_bw(base_size = 14)
-
-
-
-#################################################
-
-# Plotting PC3 scores alongside SCI result
-ggplot(data, aes(x = PC3, y = SCI))+ 
-  geom_point() +
-  xlab("PC2 score") +
-  ylab("Suture Complexity Index")+
-  theme_bw()
-
-# Fit the general linear model
-model12 <- lm(PC3 ~ SCI, data = data)
-
-# Evaulating the assumptions of the model
-par(mfrow = c(2,2))
-plot(model12) 
-mtext("Model assumptions (PC3 and SCI)", outer=T, cex=1, line=-2)
-par(mfrow = c(1,1))
-
-# To get a summary of the model
-summary(model12)
-# To get the anova summary (Pr(>F) = p-value) 
-# I don't think this anova bit is necessary
-anova(model12)
-
-# Add the model interpretation to the scatter plot (ie the line)
-ggplot(data, aes(x = PC3, y = SCI))+ 
-  geom_point(colour = "cornflowerblue") +
-  geom_smooth(method = 'lm', colour = "black", size = 1) +
-  xlab("PC3 score") +
-  ylab("Suture Complexity Index")+
-  theme_bw(base_size = 14)
-
-
-
-
-#################################################
-
-# Plotting PC3 scores alongside FD box result
-ggplot(data, aes(x = PC3, y = FD_box))+ 
-  geom_point() +
-  xlab("PC3 score") +
-  ylab("Fractal Dimension (box counting)")+
-  theme_bw()
-
-# Fit the general linear model
-model13 <- lm(PC3 ~ FD_box, data = data)
-
-# Evaulating the assumptions of the model
-par(mfrow = c(2,2))
-plot(model13) 
-mtext("Model assumptions (PC3 and FD_box)", outer=T, cex=1, line=-2)
-par(mfrow = c(1,1))
-
-# To get a summary of the model
-summary(model13)
-# To get the anova summary (Pr(>F) = p-value) 
-# I don't think this anova bit is necessary
-anova(model13)
-
-# Add the model interpretation to the scatter plot (ie the line)
-ggplot(data, aes(x = PC3, y = FD_box))+ 
-  geom_point(colour = "cornflowerblue") +
-  geom_smooth(method = 'lm', colour = "black", size = 1) +
-  xlab("PC3 score") +
-  ylab("Fractal Dimension (box counting)")+
-  theme_bw(base_size = 14)
-
-
-
-
-#################################################
-
-# Plotting PC3 scores alongside FD madogram result
-ggplot(data, aes(x = PC3, y = FD_madogram))+ 
-  geom_point() +
-  xlab("PC3 score") +
-  ylab("Fractal Dimension (madogram)")+
-  theme_bw()
-
-# Fit the general linear model
-model14 <- lm(PC3 ~ FD_madogram, data = data)
-
-# Evaulating the assumptions of the model
-par(mfrow = c(2,2))
-plot(model14) 
-mtext("Model assumptions (PC3 and FD madogram)", outer=T, cex=1, line=-2)
-par(mfrow = c(1,1))
-
-# To get a summary of the model
-summary(model14)
-# To get the anova summary (Pr(>F) = p-value) 
-# I don't think this anova bit is necessary
-anova(model14)
-
-# Add the model interpretation to the scatter plot (ie the line)
-ggplot(data, aes(x = PC3, y = FD_madogram))+ 
-  geom_point(colour = "cornflowerblue") +
-  geom_smooth(method = 'lm', colour = "black", size = 1) +
-  xlab("PC3 score") +
-  ylab("Fractal Dimension (madogram)")+
-  theme_bw(base_size = 14)
-
-
-
-
-#################################################
-
-# Plotting PC3 scores alongside PSD result
-ggplot(data, aes(x = PC3, y = PSD))+ 
-  geom_point() +
-  xlab("PC3 score") +
-  ylab("Power Spectrum Density")+
-  theme_bw()
-
-# Fit the general linear model
-model15 <- lm(PC3 ~ PSD, data = data)
-
-# Evaulating the assumptions of the model
-par(mfrow = c(2,2))
-plot(model15) 
-mtext("Model assumptions (PC3 and PSD)", outer=T, cex=1, line=-2)
-par(mfrow = c(1,1))
-
-# To get a summary of the model
-summary(model15)
-# To get the anova summary (Pr(>F) = p-value) 
-# I don't think this anova bit is necessary
-anova(model15)
-
-# Add the model interpretation to the scatter plot (ie the line)
-ggplot(data, aes(x = PC3, y = PSD))+ 
-  geom_point(colour = "cornflowerblue") +
-  geom_smooth(method = 'lm', colour = "black", size = 1) +
-  xlab("PC3 score") +
-  ylab("Power Spectrum Density")+
-  theme_bw(base_size = 14)
 
 
 
