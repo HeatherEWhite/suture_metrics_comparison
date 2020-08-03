4e166dec07b18367fba2765320d75fe3e7242b49
HeatherEWhite <46779842+HeatherEWhite@users.noreply.github.com>
2020-08-03 16:36:13 +0100

    Update collecting_suture_curves.R

--- Code_for_analyses/collecting_suture_curves.R
+++ Code_for_analyses/collecting_suture_curves.R
@@ -4,7 +4,7 @@
 
 # Date created: 21/05/19
 
-# Last modified: 23/05/19
+# Last modified: 23/11/19
 
 # License: MIT license
 
@@ -21,15 +21,18 @@ library(geomorph)
 # Initialising the app for positioning landmarks
 digitizeImages(image.file = 'Raw_Data/Photos', shapes.file = 'Raw_Data/Original_Landmarks', landmarks.ref = 'Raw_Data/Landmarks.txt', curves.ref = 'Raw_Data/Curves.txt')
 
-# Saving the original landmark data in the Analysis folder
+# Save the original landmark data
 shapedat<-readShapes("./Raw_Data/Original_Landmarks")
 
-# Saving the resampled shape data, so there are the same number of semilandmarks for each curve of each suture
+# Save the resampled shape data - each suture resampled to have 500 semi-landmarks along its path
 shapedat_resampled<-readland.shapes(shapedat, c(500))
 
-# Saving the resampled landmarks to a variable for each specimen
+# Save the resampled landmarks to a variable for each specimen
 # This has to be run each time with every specimen name
-Tragelaphus_scriptus_resampled <- shapedat_resampled[["landmarks"]][["Tragelaphus_scriptus"]]
+Tragelaphus_scriptus_resampled <- shapedat_resampled[["landmarks"]][["specimen_name"]]
 
-# Writing the resampled (500) landmark data to a csv file and saving it in the 'Data' folder rather than the raw data folder
-write.csv(Tragelaphus_scriptus_resampled, file = 'Data/Resampled_landmarks/Tragelaphus_scriptus_resampled.csv')
+# Write the resampled (500) landmark data to a .csv file
+write.csv(specimen_name_resampled, file = 'Y:/xxx/xxx/xxx.csv')
+
+
+
